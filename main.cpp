62873120b9de7da722a0136d6b9172bd3a498618
peng <3335476626@qq.com>
2021-02-15 13:18:03 +0800

    achieve sort

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
-using namespqce std;
+using namespace std;
 
 void sort(int arr[],int size)
 {
-    
+     int tmp = 0;
+     for(int i = 0;i < size;i++)
+     {
+     	for(int j = 0;j < size-1-i;j++)
+	{
+	     if(arr[j] > arr[j+1])
+	     {
+		tmp = arr[j];
+		arr[j] = arr [j+1];
+		arr[j+1] = tmp;
+	     }
+	}  
+     }     
 }
 
  int main()
